34aef4696b0e7ccdfbfc9f04df8d70b4db84064a
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2025-10-10 11:02:38 +0545

    Minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,14 @@
-'use client'
-import { Card, CardContent } from "@/components/ui/card";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
+
+import DestinationsCarousel from "@/components/DestinationsCarousel";
+import { Card } from "@/components/ui/card";
+
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import Autoplay from "embla-carousel-autoplay"
+
 
 const HomePage = () => {
-  const destinations = [
-    { title: "Everest Base Camp", img: "/images/everest.webp" },
-    { title: "Annapurna Circuit", img: "/images/annapurna.webp" },
-    { title: "Langtang Valley", img: "/images/langtang.webp" },
-    { title: "Manaslu Trek", img: "/images/manaslu.webp" },
-    { title: "Upper Mustang", img: "/images/mustang.webp" },
-  ];
+
 
   return (
     <div className="py-20 font-sans text-gray-800">
@@ -53,32 +42,7 @@ const HomePage = () => {
         <h2 className="text-3xl font-bold text-center mb-10 text-teal-600">
           Popular Treks & Destinations
         </h2>
-        <Carousel plugins={[
-          Autoplay({
-            delay:2000
-          })
-        ]} className="overflow-hidden">
-          <CarouselContent>
-            {destinations.map((place, index) => (
-              <CarouselItem key={index} className="md:basis-1/3 lg:basis-1/4">
-                <Card className="bg-white overflow-hidden rounded-2xl shadow-lg hover:scale-105 transition border-0">
-                  <Image
-                    src={place.img}
-                    alt={place.title}
-                    width={400}
-                    height={250}
-                    className="object-cover w-full h-56"
-                  />
-                  <CardContent className="p-4 text-center">
-                    <h3 className="text-lg font-semibold">{place.title}</h3>
-                  </CardContent>
-                </Card>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
+        <DestinationsCarousel/>
       </div>
 
       {/* Why Choose Us */}
---
+++ src/components/DestinationsCarousel.tsx
@@ -0,0 +1,55 @@
+'use client'
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import Image from "next/image";
+import { Card, CardContent } from "./ui/card";
+
+const DestinationsCarousel = () => {
+  const destinations = [
+    { title: "Everest Base Camp", img: "/images/everest.webp" },
+    { title: "Annapurna Circuit", img: "/images/annapurna.webp" },
+    { title: "Langtang Valley", img: "/images/langtang.webp" },
+    { title: "Manaslu Trek", img: "/images/manaslu.webp" },
+    { title: "Upper Mustang", img: "/images/mustang.webp" },
+  ];
+  return (
+    <Carousel
+      plugins={[
+        Autoplay({
+          delay: 2000,
+        }),
+      ]}
+      className="overflow-hidden"
+    >
+      <CarouselContent>
+        {destinations.map((place, index) => (
+          <CarouselItem key={index} className="md:basis-1/3 lg:basis-1/4">
+            <Card className="bg-white overflow-hidden rounded-2xl shadow-lg hover:scale-105 transition border-0">
+              <Image
+                src={place.img}
+                alt={place.title}
+                width={400}
+                height={250}
+                className="object-cover w-full h-56"
+              />
+              <CardContent className="p-4 text-center">
+                <h3 className="text-lg font-semibold">{place.title}</h3>
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+};
+
+export default DestinationsCarousel;
