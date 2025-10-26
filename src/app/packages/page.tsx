099d63d8ec12c5071605089004918e6de1d3397e
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2025-10-26 10:24:19 +0545

    Suspense Error Fix

--- src/app/packages/page.tsx
+++ src/app/packages/page.tsx
@@ -1,11 +1,13 @@
 import ViewPackages from '@/components/packages/ViewPackages'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const PackagesPage = () => {
   return (
     <div className='py-40 px-10 space-y-5'>
         <h1 className='text-xl text-teal-700 font-bold'>All Packages</h1>
-        <ViewPackages/>
+        <Suspense>
+            <ViewPackages/>
+        </Suspense>
     </div>
   )
 }
