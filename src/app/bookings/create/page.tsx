05bf994a898c4a28329d4af5bdfb7108ab290bff
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2025-10-31 22:54:26 +0545

    Build Error Fix

--- src/app/bookings/create/page.tsx
+++ src/app/bookings/create/page.tsx
@@ -1,11 +1,13 @@
 import CreateBooking from '@/components/bookings/CreateBooking'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const CreateBookingPage = () => {
   return (
     <div className='py-40 px-10 space-y-5'>
         <h1 className='text-xl text-teal-700 font-bold'>Create a new booking</h1>
-        <CreateBooking/>
+        <Suspense>
+            <CreateBooking/>
+        </Suspense>
     </div>
   )
 }
